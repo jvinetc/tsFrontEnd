225b89bb6b5a4f48dbaa0a217f0e4c2628ed7274
Jose Vinet <jvinetc@gmail.com>
2025-08-26 18:34:45 -0400

    correccion de navegacion

--- src/Pages/Logout.tsx
+++ src/Pages/Logout.tsx
@@ -10,7 +10,7 @@ const Logout = () => {
   setUser(undefined);
   setIsLoggedIn(false);
   sessionStorage.removeItem('token');
-  navigate('/login');
+  navigate('/');
 }, []);
   return (
     <div>Logout</div>
